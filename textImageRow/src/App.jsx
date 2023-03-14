138e93cb6cd4de042a7536ca22a99f200991439a
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-03-14 12:02:58 +0100

    first commit

---
+++ textImageRow/src/App.jsx
@@ -0,0 +1,17 @@
+
+// import Test from './Components/Test'
+import TextImage from './Components/TextImage'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+      <TextImage/>
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ textImageRow/src/Components/TextImage.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+
+// Import the stylesheet here 
+import '../Styles/TextImage.css'; 
+
+
+function TestImage() {
+  return (
+  
+  <div className="positionContainer">
+  
+    <div className="mainContainer" >
+    
+ 
+    {/* <div className='sectionTitleContainer' >
+    <h2 className='sectionTitle' >
+    
+    Inspiring Title   
+    </h2>
+    </div> */}
+    
+    
+    {/* Add in the image container here  */}
+    <div className="imageContainer">
+    Image Container
+    </div>
+    
+    {/* Add in the text container here  */}
+    <div className="textContainer">
+    
+    {/* Add in the header container here  */}
+    <div className="headerContainer">
+    <h3 className="header" >
+    Inspiring Title 
+    </h3>
+    </div>
+    
+    
+    {/* Add in the paragraph container here  */}
+    <div className="paragraphContainer">
+    
+    {/* Add in the text here  */}
+    <p className="paragraph">
+    
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto voluptates dolore explicabo porro voluptatibus sequi fugit atque accusantium et temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laboriosam suscipit iste aliquam cumque quidem quae repellat, provident velit aspernatur, distinctio ipsum fugiat ex alias culpa magnam labore saepe, dignissimos eveniet exercitationem quas? In eligendi soluta rem quo praesentium tempora sunt odit adipisci iusto, vel iste blanditiis et ipsam numquam nobis ipsum aut placeat fugit earum animi hic officiis. Deserunt rem asperiores quia quaerat necessitatibus architecto modi accusantium quisquam atque a suscipit, nisi voluptate corrupti quae voluptates voluptatem cumque! Deserunt, in similique, laborum distinctio odit, voluptatum ut doloribus maiores id adipisci optio quos velit. Quae, molestias. Voluptatibus aliquid velit molestiae.
+    </p>
+    
+    </div>
+    
+    </div>
+    
+    </div>
+  </div>
+  );
+  
+}
+
+export default TestImage
